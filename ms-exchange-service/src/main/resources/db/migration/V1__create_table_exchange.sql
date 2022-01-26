1013b39fd72e55392e5f0193d74b9513978e6514
Thiago Rodrigues <thiagor_@live.com>
2022-01-25 22:53:06 -0300

    fix: rename exchange table

--- ms-exchange-service/src/main/resources/db/migration/V1__create_table_exchange.sql
+++ ms-exchange-service/src/main/resources/db/migration/V1__create_table_exchange.sql
@@ -1,6 +1,15 @@
-CREATE TABLE cambio (
+CREATE TABLE tb_exchange (
   id INT(10) AUTO_INCREMENT PRIMARY KEY,
   from_currency CHAR(3) NOT NULL,
   to_currency CHAR(3) NOT NULL,
   conversion_factor decimal(65,2) NOT NULL
 );
+
+INSERT INTO tb_exchange (from_currency, to_currency, conversion_factor)
+    VALUES ('USD', 'BRL', 5.73),
+			('USD', 'EUR', 0.84),
+			('USD', 'GBP', 0.73),
+			('USD', 'ARS', 92.56),
+			('USD', 'CLP', 713.30),
+			('USD', 'COP', 3665.00),
+			('USD', 'MXN', 20.15);
