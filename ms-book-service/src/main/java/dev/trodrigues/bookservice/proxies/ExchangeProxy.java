d0e5def72d6e4d4435e167472dd6175673bc1bc0
Thiago Rodrigues <thiagor_@live.com>
2022-01-26 21:21:50 -0300

    chore: feign use naming server

--- ms-book-service/src/main/java/dev/trodrigues/bookservice/proxies/ExchangeProxy.java
+++ ms-book-service/src/main/java/dev/trodrigues/bookservice/proxies/ExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange-service", url = "localhost:8000")
+@FeignClient(name = "exchange-service")
 public interface ExchangeProxy {
 
     @GetMapping("/exchange-service/{amount}/{from}/{to}")
